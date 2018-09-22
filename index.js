f888fff29930be9adc911bfece81619ab2e36a30
Floteckk <43255189+Floteckk@users.noreply.github.com>
2018-09-23 01:29:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
 
 var bot = new Discord.Client();
-
+const token = process.env.token
 bot.on('guildMemberAdd', async member => {
 const channel = member.guild.channels.find('name', 'général');
 if (!channel) return;
 channel.send(`:tada: Bienvenue sur le Discord du clan mon chere Kubrow ! ${member} :tada:`);
 
 });
-bot.login('NDg5OTE0MzQ3NTQwMzE2MTkw.DnyvOQ.0n4LtJ2KYSM3g7sKZUHsHfN-eYE');
+bot.login(token);
