45430fcab91d4624ac95dcc34ad453fa6bc57e83
Floteckk <43255189+Floteckk@users.noreply.github.com>
2018-09-23 00:59:16 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,12 +1,10 @@
 const Discord = require('discord.js')
-
-const client = new Discord.Client()
-
+var bot = new Discord.Client()
 const prefix = 'Lotus'
 
-client.login('NDg5OTE0MzQ3NTQwMzE2MTkw.DnyvOQ.0n4LtJ2KYSM3g7sKZUHsHfN-eYE')
+bot.login('NDg5OTE0MzQ3NTQwMzE2MTkw.DnyvOQ.0n4LtJ2KYSM3g7sKZUHsHfN-eYE')
 
-client.on('guildMemberAdd', async member => {
+bot.on('guildMemberAdd', async member => {
 const channel = member.guild.channels.find('name', 'général');
 if (!channel) return;
 channel.send(`:tada: Bienvenue sur le Discord du clan mon chere Kubrow ! ${member} :tada:`);
